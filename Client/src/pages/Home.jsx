07fe780935515449672ffd05188f3b9bb8454130
SaiHarsha502vvit <21bq1a0502@vvit.net>
2025-05-15 11:57:14 +0530

    okey completed login and Signup funcitonalities

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,17 +1,30 @@
-import '../styles/Home.css'
+import "../styles/Home.css";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Home(){
-  return(
-    <div className='home-container'>
-        <h1 className='h-heading'>Welcome to <span className='headi'>Transfer File.</span></h1>
+function Home() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/login");
+    }
+    // Optionally, verify token with backend here
+  }, [navigate]);
 
-        <div className='files'>
-          <div className='request-container'>
-            <button>Send files</button>
-          </div>
+  return (
+    <div className="home-container">
+      <h1 className="h-heading">
+        Welcome to <span className="headi">Transfer File.</span>
+      </h1>
+
+      <div className="files">
+        <div className="request-container">
+          <button>Send files</button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
